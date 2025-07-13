2cee693fe6a88a10f3921b393609842760215481
UltraEpicBanana <tevclairefond@outlook.fr>
2025-07-13 11:35:17 +0200

    Create setup_logwatch.sh

---
+++ setup_logwatch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+apt install -y logwatch rsyslog
+
+# Add auth logs for rsyslog
+echo "auth,authpriv.* /var/log/auth.log" > /etc/rsyslog.d/20-default.conf
+
+# Create Logwatch config for sshd
+mkdir -p /etc/logwatch/conf/logfiles
+cat <<EOF > /etc/logwatch/conf/logfiles/sshd.conf
+LogFile = auth.log
+*RemoveHeaders
+EOF
+
+# Restart rsyslog to apply changes
+systemctl restart rsyslog
