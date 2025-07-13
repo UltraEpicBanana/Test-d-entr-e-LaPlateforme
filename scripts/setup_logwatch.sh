6c37b8b0287436d8410498db149c0fc78d44a292
UltraEpicBanana <tevclairefond@outlook.fr>
2025-07-13 12:38:52 +0200

    Update setup_logwatch.sh

--- scripts/setup_logwatch.sh
+++ scripts/setup_logwatch.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+# logwatch ne peut pas acceder à systemctl, c'est pourquoi nous avons besoin de rsyslog
 apt install -y logwatch rsyslog
 
-# Ajout des autorisations des logs pour rsyslog
+# Ajout des autorisations pour que rsyslog enregistre les logs dans un fichier auth.log que logwatch pourra lire
 echo "auth,authpriv.* /var/log/auth.log" > /etc/rsyslog.d/20-default.conf
 
-# Create Logwatch config for sshd
+# Creation du fichier sshd.conf pour indiquer à logwatch d'aller lire le fichier auth.log
 mkdir -p /etc/logwatch/conf/logfiles
 cat <<EOF > /etc/logwatch/conf/logfiles/sshd.conf
 LogFile = auth.log
 *RemoveHeaders
 EOF
 
-# Restart rsyslog to apply changes
+# Restart de rsyslog pour appliquer les changements 
 systemctl restart rsyslog
