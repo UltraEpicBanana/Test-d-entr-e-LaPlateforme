7765740c75466a56a7e30c8dfd623cfa1575d5a5
UltraEpicBanana <tevclairefond@outlook.fr>
2025-07-13 12:45:40 +0200

    Delete scripts/cron_logwatch.sh

--- scripts/cron_logwatch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-OUTPUT_DIR="/var/log/logwatch"
-mkdir -p "$OUTPUT_DIR"
-OUTPUT_FILE="$OUTPUT_DIR/logwatch-$(date +%F).log"
-
-/usr/sbin/logwatch --range yesterday --service sshd --service http --output file --format text --filename "$OUTPUT_FILE"
